031f75a08b44c02a0ae4437256f14966e77bc782
Pawel Siemienik <p.siemienik@ohgames.net>
2018-12-10 19:51:59 +0100

    Remove and ignore unnecessary files
    Todo: 1. BUG: BaseCell do not rewrite formulas and sharedFormulas

--- src/example.js
+++ src/example.js
@@ -6,34 +6,61 @@ import {Workbook} from 'exceljs'
 const renderer = new Renderer(new CellTemplatePool());
 /*/
 const renderer = new Renderer(new DebugCellTemplatePool());
+
 //*/
+class LocalisationViewModel {
+    constructor(localisation) {
+        this.id_sl = 'id_sl';
+        this.network = 'network';
+        this.province = 'province';
+        this.city = 'city';
+        this.address = 'address';
+        this.onlineCard = null;
+        this.shoppingCenter = 'shoppingCenter';
+        this.audience = 'audience';
+        this.totalDisplays = 'totalDisplays';
+        this.g8 = 'g8';
+        this.g10 = 'g10';
+        this.g12 = 'g12';
+        this.agglomeration = 'agglomeration';
+        this.population = 'population';
+        this.outpost = 'outpost';
+        this.displaySize = 'displaySize';
+        this.spotResolution = 'spotResolution';
+        this.realResolution = 'realResolution';
+        this.soft = 'soft';
+        this.trigger = 'trigger';
+        this.postCode = 'postCode';
+        this.coordinates = 'coordinates';
+        this.led = 'led';
+        this.openHour = 'openHour';
+        this.banPoliticial = 'banPoliticial';
+    }
+
+}
+
+class CalculatorViewModel {
+    constructor(localisations) {
+        this.onlineCardLabel = "Karta on-line";
+        this.localisations = localisations.map(x => new LocalisationViewModel(x));
+    }
+}
 
 (async () => {
+    const locs = [];
+    locs.push({});
+    locs.push({});
+    locs.push({});
+    locs.push({});
+    locs.push({});
+    locs.push({});
+    locs.push({});
+    locs.push({});
     const result = await renderer.render(async () => {
         const template = new Workbook();
-        return await template.xlsx.readFile('../template.xlsx');
-    }, {
-        name: "Pierwszy",
-        super: {test: 123},
-        items: [
-            // {firstName: "Paweł", secondName: "Siemienik"},
-            {firstName: "Other", secondName: "Siemie"},
-            {firstName: "Other", secondName: "Stranger"},
-            {firstName: "Paweł", secondName: "Siemienik"},
-            {firstName: "Other", secondName: "Siemie"},
-            {firstName: "Other", secondName: "Stranger"},
-            {firstName: "Paweł", secondName: "Siemienik"},
-            {firstName: "Other", secondName: "Siemie"},
-            {firstName: "Other", secondName: "Stranger"},
-            // {firstName: "Paweł", secondName: "Siemienik"},
-            // {firstName: "Other", secondName: "Siemie"},
-            // {firstName: "Other", secondName: "Siemie"},
-            // {firstName: "Other", secondName: "Stranger"},
-            // {firstName: "Paweł", secondName: "Siemienik"},
-            // {firstName: "Other", secondName: "Siemie"},
-            // {firstName: "LAST", secondName: "Stranger"},
-        ]
-    });
+        const workbook = await template.xlsx.readFile('./calculator-template.xlsx');
+        return workbook;
+    }, new CalculatorViewModel(locs));
 
-    await result.xlsx.writeFile('../out.xlsx');
+    await result.xlsx.writeFile('./out-calc.xlsx');
 })();
