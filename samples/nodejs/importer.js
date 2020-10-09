ba224a5d0572c6f63a7bb27cee4833da80fb3ef9
@Siemienik Paweł <siemienik.pawel@gmail.com>
2020-10-09 03:08:56 +0200

    Upgrade sample from v2.3.0 to v2.3.1 and resolve deprecations.

--- samples/nodejs/importer.js
+++ samples/nodejs/importer.js
@@ -1,4 +1,4 @@
-const ImporterFactory = require('xlsx-import/lib/ImporterFactory').default;
+const { ImporterFactory } = require('xlsx-import/lib/ImporterFactory');
 const { getInvoiceConfig } = require('./configs/invoiceConfig');
 
 const factory = new ImporterFactory();
@@ -6,12 +6,12 @@ const factory = new ImporterFactory();
 const importInvoice = async (invoicePath) => {
     const config = getInvoiceConfig();
 
-    const importer = await factory.From(invoicePath);
+    const importer = await factory.from(invoicePath);
 
-    const {date, dueDate} = importer.GetAllItems(config.misc)[0];
-    const seller = importer.GetAllItems(config.seller)[0];
-    const buyer = importer.GetAllItems(config.buyer)[0];
-    const items = importer.GetAllItems(config.items);
+    const {date, dueDate} = importer.getAllItems(config.misc)[0];
+    const seller = importer.getAllItems(config.seller)[0];
+    const buyer = importer.getAllItems(config.buyer)[0];
+    const items = importer.getAllItems(config.items);
 
     const total = items.reduce((p, c) => p + c.price, 0);
 
