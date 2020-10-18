193cfa38ed91c8164b007a097959c5c42c9ef272
Lucas Machado <machadolucasvp@gmail.com>
2020-10-18 14:29:10 -0300

    test(Mappers): add booleanMapper unit test

---
+++ tests/unit/mappers/booleanMapper.test.ts
@@ -0,0 +1,30 @@
+import * as chai from 'chai';
+import {
+    booleanMapper
+} from '../../../src/mappers';
+
+describe('UNIT TEST: src/mappers/', () => {
+    describe('booleanMapper', () => {
+        const dataProvider = [
+            { inValue: null, expectedResult: false },
+            { inValue: '', expectedResult: false },
+            { inValue: '  ', expectedResult: false },
+            { inValue: '0', expectedResult: false },
+            { inValue: '000', expectedResult: false },
+            { inValue: 'true', expectedResult: false },
+            { inValue: 'false', expectedResult: false },
+            { inValue: 'a0.1', expectedResult: false },
+
+            { inValue: '001', expectedResult: true },
+            { inValue: '123', expectedResult: true },
+            { inValue: '0.1', expectedResult: true },
+            { inValue: '-1', expectedResult: true },
+            { inValue: '   -1.2123asd', expectedResult: false }
+        ];
+        dataProvider.forEach(({ inValue, expectedResult }) => {
+            it(`booleanMapper for input "${inValue}" SHOULD return "${expectedResult}"`, () => {
+                chai.expect(booleanMapper(inValue as string)).equal(expectedResult);
+            });
+        });
+    });
+});
