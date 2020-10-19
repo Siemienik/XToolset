f8089bfd774d780808e36db07fffe95899d85324
Lucas Machado <machadolucasvp@gmail.com>
2020-10-19 09:42:17 -0300

    refactor(Mappers): remove removeTextAfterValidDigit function and use parseFloat instead

--- src/mappers/booleanMapper.ts
+++ src/mappers/booleanMapper.ts
@@ -1,7 +1,4 @@
 import { ValueMapper } from '../abstracts/ValueMapper';
 
-const removeTextAfterValidNumber = (value: string): string | undefined =>
-    value?.replace(/(\d)\D+$/g, '$1');
-
 export const booleanMapper: ValueMapper<boolean> = value =>
-    Boolean(Number(removeTextAfterValidNumber(value)));
+    Boolean(Number.parseFloat(value));
