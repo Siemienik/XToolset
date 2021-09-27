2082aa38f4b890e438ab299b4257e92bdc72329f
Siemienik Pawel <siemienik.pawel@gmail.com>
2021-09-27 21:55:35 +0200

    Added dateMapper with protection against invalid value passes as valid. (https://github.com/Siemienik/XToolset/pull/192)
    ref: https://github.com/Siemienik/XToolset/issues/58

---
+++ packages/xlsx-import/src/mappers/dateMapper.ts
@@ -0,0 +1,4 @@
+import { ValueMapper } from '../abstracts/ValueMapper';
+
+// noinspection SuspiciousTypeOfGuard
+export const dateMapper: ValueMapper<Date> = value => new Date(typeof value === "string" ? value : "invalid");
