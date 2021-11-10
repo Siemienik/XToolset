70a98b73e05890f8a01fa36ab0d874c3b0cceb06
maryiabydanova <mariabydanova@gmail.com>
2021-11-10 14:50:09 -0800

    Issue 81: Added getFirstItem method (#198)
    * feat: getFirstItem method for getting the value of the first cell of sheet

--- packages/xlsx-import/src/IImporter.ts
+++ packages/xlsx-import/src/IImporter.ts
@@ -2,6 +2,7 @@ import { ISourceConfig } from './config/ISourceConfig';
 
 export interface IImporter {
     getAllItems<T>(config: ISourceConfig): T[];
+    getFirstItem<T>(config: ISourceConfig): T;
 }
 
 /** @deprecated Default exports will be removed in January 2021. Please to use brackets (`{ IImporter }`). */
