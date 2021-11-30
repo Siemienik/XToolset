17b3116eb4368a95356238f79d6f1660f5fa25a0
Maryia Bydanava <mariabydanova@gmail.com>
2021-11-30 11:09:18 -0800

    feat: added customizable default value for jsonMapper

--- packages/xlsx-import/src/mappers/jsonMapper.ts
+++ packages/xlsx-import/src/mappers/jsonMapper.ts
@@ -1,9 +1,33 @@
-type JsonMapper = <TJsonResult>(value: string) => TJsonResult | null;
 
-export const jsonMapper: JsonMapper = <TJsonResult extends any>(value: string): TJsonResult | null => {
-    try {
-        return JSON.parse(value);
-    } catch (e) {
-        return null;
+type callSignature = <TJsonResult, TDefaultResult>(value: string) => TJsonResult | TDefaultResult;
+
+type JsonMapper = {
+    default: <TDefaultResult>(defaultResult: TDefaultResult) => JsonMapper
+} & callSignature;
+
+interface IJsonMapperOptions {
+    default: any;
+}
+
+const factory = (options: Readonly<IJsonMapperOptions>): JsonMapper => {
+    const mapper = (json: string) => {
+        try {
+            return JSON.parse(json);
+        } catch (e) {
+            return options.default
+        }
     }
+
+    mapper.default = <TDefaultResult>(defaultResult: TDefaultResult) => factory({
+        ...options,
+        default: defaultResult
+    });
+
+    return mapper;
 };
+
+
+
+export const jsonMapper: JsonMapper = factory({
+    default: null,
+});
