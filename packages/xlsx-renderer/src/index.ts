7eabcdb08f7eee08ae322a9e4604325acc73274d
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-12-07 20:42:09 +0100

    WIP: Introducing webpack to create a bundle available to publish via CDN TODO:  * [ ] licences listing  * [ ] examples  * [ ] choose cdn provider  * [ ] publishing bundle ( I want to avoid publishing via npm or gh)

---
+++ packages/xlsx-renderer/src/index.ts
@@ -0,0 +1,4 @@
+export { Renderer } from './Renderer'
+export { CellTemplatePool } from './CellTemplatePool'
+export { CellTemplateDebugPool } from './CellTemplateDebugPool'
+
---
+++ packages/xlsx-renderer/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, './src/index.ts'),
+    mode: 'production',
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    output: {
+        clean: true,
+        library: 'xlsxRenderer',
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        path: path.resolve(__dirname, 'bundle'),
+        filename: 'xlsx-renderer.full.js',
+    },
+};
