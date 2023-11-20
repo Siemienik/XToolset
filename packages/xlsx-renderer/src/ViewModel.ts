65c4f959e807d182f16c8dfd2893e094c1cb8692
@Siemienik Paweł <siemienik.pawel@gmail.com>
2023-11-20 23:14:55 +0100

    Using Proxy to prevent against modification the original view model.
    Fixes https://github.com/Siemienik/XToolset/issues/137
    Fixes https://github.com/Siemienik/XToolset/issues/229

--- packages/xlsx-renderer/src/ViewModel.ts
+++ packages/xlsx-renderer/src/ViewModel.ts
@@ -1,1 +1,22 @@
 export type ViewModel = any;
+
+export const createVmProxyHandler = ()=> {
+    const data: Record<string | number, unknown> = {};
+
+    return {
+        get(target: any, p: PropertyKey): any {
+            if (typeof p !== 'string' && typeof p !== 'number') {
+                return;
+            }
+            return p in data ? data[p] : target[p]
+        },
+        set(target: unknown, p: PropertyKey, value: unknown): boolean {
+            if (typeof p !== 'string' && typeof p !== 'number') {
+                return false;
+            }
+            data[p] = value
+
+            return true
+        },
+    }
+}
---
+++ packages/xlsx-renderer/tests/spec/cell-value-type.test.ts
@@ -0,0 +1,25 @@
+import * as chai from 'chai';
+import { CellValue, Workbook } from 'exceljs';
+import { ValueType } from 'exceljs';
+import { Renderer } from '../../src/Renderer';
+
+const WS_NAME = 'test ws';
+
+describe('BaseCell unit tests', () => {
+    const factory = async ():Promise<Workbook> =>{
+        const template = new Workbook();
+        template.addWorksheet(WS_NAME).addRow(['## testVar', "#! FINISH"]);
+
+        return template;
+    }
+
+    it('DateTime', async () => {
+        const viewModel = {
+            testVar: new Date(2023, 11, 17, 21, 37)
+        }
+        const renderer = new Renderer();
+        const output = await renderer.render(factory, viewModel)
+
+        chai.expect(output.getWorksheet(WS_NAME)?.getCell('A1').type).equals(ValueType.Date);
+    });
+});
